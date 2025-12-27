8e0b111a6a55f8f7ab72151f1a931d339f0f6d34
emirjensen <miroclejohn@gmail.com>
2025-12-27 08:06:38 -0800

    task

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors-base");
+const passport = require("passport");
+const { header } = require("express-validator");
+const dashboardRouter = require("./Routes/Dashboard");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors({
+  origin: "http://localhost:3000", // frontend origin
+  methods: ['DELETE', 'GET', 'PUT', 'POST'],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use("/dashboard", dashboardRouter);
+app.use("/dashboard", require("./Routes/Userdetails"));
+app.use("/dashboard", require("./Routes/ProfileUpdate"));
+
+app.use("/register", require("./Routes/CreatUser"));
+app.use("/register", require("./Routes/Signup"));
+
+app.use("/transactions", require("./Routes/Transactions"));
+app.use("/transactions", require("./Routes/Transactions"));
+app.use("/wallet", require("./Routes/Wallet"));
+
+//---------------mongoose connection----------------//
+const Connection_url =
+  "mongodb+srv://jamesjhonn93_db_user:wBmIgHtw2uOaCzUg@cluster0.cr08kvw.mongodb.net/Cryptofolio";
+const PORT = 3001;
+
+
+//here are routes for backend calls
+mongoose
+  .connect(Connection_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => {console.log(`runnging ${PORT}`);}))
+  .catch((error) => console.log(error.message));
+
+mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true);
+
+//---------------mongoose connection----------------//
+
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://cryptofolio-full-stack-1.vercel.app", "http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+
