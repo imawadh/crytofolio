31cd86198ef21b6e901bc992a305737d975078b8
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2026-01-01 01:48:39 +0530

    Frontend Ready to deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BASE_URL: process.env.REACT_APP_BASE_URL || "http://localhost:5000",
+  BASE_URL: process.env.REACT_APP_BASE_URL || "https://task-cryptopolio.onrender.com",
 };
 
 export default config;
